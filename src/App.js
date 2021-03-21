81bdc2e3cc194400705fb0ba93e20900da008a49
InteractiveNinja <gabrielnadolny2@gmail.com>
2021-03-21 17:29:21 +0100

    desktop device register done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    userid: ""
+  };
+
+  handleInput = event => {
+    this.setState({ userid: event.target.value });
+  };
+
+  connectWS = () => {
+    let con = new WebSocket("ws://localhost:8080")
+    con.onopen = () =>{
+        con.send(`{"type":"register","value":"${this.state.userid}"}`)
+    }
+    con.onmessage = (msg) =>{
+        let json = JSON.parse(msg.data)
+        switch (json.type) {
+            case "callback":
+                if(json.value === "200") alert("Regestrierung Erfolgreich")
+                if(json.value === "500") alert("Fehlgeschlagen")
+                break;
+            case "show":
+                alert(json.value)
+                break;
+            default:
+                alert("idk irgendwas bekommen")
+                break;
+        }
+       
+    }
+  };
+
+  render() {
+    return (
+      <div>
+          <h1>Device Register</h1>
+        <input onChange={this.handleInput} placeholder="User ID" />
+        <button onClick={this.connectWS}>Register</button>
+      </div>
+    );
+  }
 }
 
 export default App;
